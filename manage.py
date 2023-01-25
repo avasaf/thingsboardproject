0da74daf604f7a3d7e01ee909315b8ab0df0d017
avasaf <114611531+avasaf@users.noreply.github.com>
2023-01-25 12:13:18 +0100

    commands update
    updated commands

--- manage.py
+++ manage.py
@@ -2,8 +2,39 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management.base import BaseCommand
 from mqtt_read_client import receive_data
 from mqtt_write_client import send_data
+from pymodbus.client import ModbusTcpClient
+
+
+class read_modbus_data(BaseCommand):
+    help = 'Read modbus data'
+
+    def add_arguments(self, parser):
+        parser.add_argument('ip', type=str, help='IP address of the modbus device')
+
+    def handle(self, *args, **options):
+        ip = options['ip']
+        client = ModbusTcpClient(ip)
+        client.connect()
+        result = client.read_holding_registers(0, 10)
+        print(result.registers)
+        client.close()
+
+
+class write_modbus_data(BaseCommand):
+    help = 'Write modbus data'
+
+    def add_arguments(self, parser):
+        parser.add_argument('ip', type=str, help='IP address of the modbus device')
+
+    def handle(self, *args, **options):
+        ip = options['ip']
+        client = ModbusTcpClient(ip)
+        client.connect()
+        client.write_register(0, 42)
+        client.close()
 
 
 def main():
